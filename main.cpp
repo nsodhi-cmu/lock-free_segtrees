d60173d58a5ae0cff496b4af404128bdda175956
Neal K Sodhi <nsodhi@ghc67.ghc.andrew.cmu.edu>
2024-11-19 16:58:05 -0500

    file setup

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "segment_tree.h"
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
---
+++ src/segment_tree.h
@@ -0,0 +1,11 @@
+#ifndef SEGTREE_H
+#define SEGTREE_H
+
+class SegmentTree {
+    private:
+    public:
+        SegmentTree(int n);
+        ~SegmentTree();
+};
+
+#endif
